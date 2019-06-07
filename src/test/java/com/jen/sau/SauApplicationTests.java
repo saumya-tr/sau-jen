922103e3a0e580fbe14489ef1f2f963b49ce678a
kingritesh1 <vinod@aptus.in>
2019-06-07 15:29:02 +0530

    first commit

---
+++ src/test/java/com/jen/sau/SauApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jen.sau;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SauApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
