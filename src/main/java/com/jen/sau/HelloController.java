3ec91f4b3a2fb94116605df3dc306d08ba35e61d
Saumya Tripathi <saumya3.tripathi@in.ey.com>
2019-06-19 12:44:16 +0530

    Demo commit

--- src/main/java/com/jen/sau/HelloController.java
+++ src/main/java/com/jen/sau/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Welcome To The DevOps Demo";
+        return "Welcome To The Spring boot DevOps Demo";
     }
 
 }
